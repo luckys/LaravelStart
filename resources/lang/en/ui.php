a7fdcc832a7987d52b05608ccfa3ef23f9835029
luckys <cfour@openmailbox.org>
2015-07-12 21:50:17 +0100

    Initial commit

--- resources/lang/en/ui.php
+++ resources/lang/en/ui.php
@@ -2,8 +2,7 @@
 
 return [
 
-    'title'             =>  'RentCar',
-
+    'title'             =>  'LaravelStart',
     'header_top'        =>  [
         'change_password'       =>  'Cambiar Contraseña',
         'logout'        =>  'Salir'
@@ -11,16 +10,6 @@ return [
 
     'sidebar'           =>  [
         'dashboard'     =>  'Dashboard',
-        'label_car'     =>  'Vehículos',
-        'cars'          =>  'Coches',
-        'colors'        =>  'Colores',
-        'brands'        =>  'Marca',
-        'prototypes'    =>  'Modelo',
-        'conditions'    =>  'Estado',
-        'clients'       =>  'Clientes',
-        'countries'     =>  'Países',
-        'agreements'    =>  'Contratos',
-        'status'        =>  'Estados del Contrato',
         'admin_users'   =>  'Administrar Usuarios',
         'permissions'   =>  'Permisos',
         'roles'         =>  'Roles',
--- resources/lang/es/ui.php
+++ resources/lang/es/ui.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'title'             =>  'RentCar',
+    'title'             =>  'LaravelStart',
 
     'header_top'        =>  [
         'change_password'       =>  'Cambiar Contraseña',
@@ -11,16 +11,6 @@ return [
 
     'sidebar'           =>  [
         'dashboard'     =>  'Dashboard',
-        'label_car'     =>  'Vehículos',
-        'cars'          =>  'Coches',
-        'colors'        =>  'Colores',
-        'brands'        =>  'Marca',
-        'prototypes'    =>  'Modelo',
-        'conditions'    =>  'Estado',
-        'clients'       =>  'Clientes',
-        'countries'     =>  'Países',
-        'agreements'    =>  'Contratos',
-        'status'        =>  'Estados del Contrato',
         'admin_users'   =>  'Administrar Usuarios',
         'permissions'   =>  'Permisos',
         'roles'         =>  'Roles',
